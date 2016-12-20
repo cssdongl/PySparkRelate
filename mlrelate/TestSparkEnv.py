f01e126deea4ce587610dde10db427e8d9af0245
SynLz <cssdongl@163.com>
2016-12-20 14:52:43 +0800

    create pyspark

---
+++ mlrelate/TestSparkEnv.py
@@ -0,0 +1,12 @@
+from pyspark import SparkContext
+sc = SparkContext("local[2]", "First Spark App")
+data = sc.textFile("file:///home/dongl/sparkJars/pySparkScripts/testFiles/userPhcaseHistory.txt").map(lambda line:line.split(",")).map(lambda record: (record[0], record[1], record[2]))
+numPurchases = data.count()
+uniqueUsers = data.map(lambda record: record[0]).distinct().count()
+totalRevenue = data.map(lambda record: float(record[2])).sum()
+products = data.map(lambda record: (record[1], 1.0)).reduceByKey(lambda a, b: a + b).collect()
+mostPopular = sorted(products, key=lambda x: x[1], reverse=True)[0]
+print "Total purchases: %d" % numPurchases
+print "Unique users: %d" % uniqueUsers
+print "Total revenue: %2.2f" % totalRevenue
+print "Most popular product: %s with %d purchases" % (mostPopular[0], mostPopular[1])
